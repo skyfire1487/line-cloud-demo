1b193b560ff8c9d16c7b6b7aaac8cdfe19e43a3e
skyfire1487 <skyfire1487@gmail.com>
2025-12-23 13:44:13 +0800

    PORT = process.env.PORT || 3000

--- server.js
+++ server.js
@@ -1,6 +1,9 @@
 // 使用 Node.js 內建的 http 模組（不需要安裝任何套件）
 const http = require("http");
 
+// Render 會提供 PORT，沒提供時才用 3000（本機用）
+const PORT = process.env.PORT || 3000;
+
 // 建立一個 server
 const server = http.createServer((req, res) => {
   // 不管使用者打什麼網址，都回 OK
@@ -8,7 +11,7 @@ const server = http.createServer((req, res) => {
   res.end("OK");
 });
 
-// 讓 server 在 3000 port 啟動
-server.listen(3000, () => {
-  console.log("Server is running at http://localhost:3000");
+// 讓 server 在指定的 PORT 啟動
+server.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
 });
